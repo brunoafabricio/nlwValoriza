e2c4f76771ea89ad05e95eeabaaf90508a03baba
Bruno <brunof.tech@gmail.com>
2021-06-23 23:26:44 -0300

    Alter User, Add column and fix issues

--- src/database/migrations/1624501023635-AlterUserAddPassword.ts
+++ src/database/migrations/1624501023635-AlterUserAddPassword.ts
@@ -1,11 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 
 export class AlterUserAddPassword1624501023635 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            "users",
+            new TableColumn({
+                name: "password",
+                type: "varchar",
+                isNullable: true,
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users", "password")
     }
 
 }
