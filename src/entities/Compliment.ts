72e34e29d1e36151b19e14f76fb3d8da1116c297
Bruno <brunof.tech@gmail.com>
2021-06-24 12:48:36 -0300

    Relationship between Columns

--- src/entities/Compliment.ts
+++ src/entities/Compliment.ts
@@ -1,8 +1,10 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn} from "typeorm";
+import { Entity, PrimaryColumn, Column, CreateDateColumn, JoinColumn, ManyToOne} from "typeorm";
 import { v4 as uuid } from "uuid";
+import { Tag } from "./Tag";
+import { User } from "./User";
 
 @Entity("compliments")
-class Compliment{
+class Compliment {
   @PrimaryColumn()
   //readonly id: string;
   id: string;
@@ -10,18 +12,29 @@ class Compliment{
   @Column()
   user_sender: string;
 
+  @JoinColumn({name: "user_sender"})
+  @ManyToOne(() => User)
+  userSender: User
+
   @Column()
   user_receiver: string;
 
+  @JoinColumn({name: "user_receiver"})
+  @ManyToOne(() => User)
+  userReceiver: User
+
   @Column()
   tag_id: string;
+  
+  @JoinColumn({name: "tag_id"})
+  @ManyToOne(() => Tag)
+  tag: Tag;
 
   @Column()
   message: string;
 
   @CreateDateColumn()
   created_at: Date;
-
 
   contructor() {
     if(!this.id){
@@ -31,3 +44,11 @@ class Compliment{
 }
 
 export { Compliment }
+
+/**
+ * Relacionamentos: 
+ *  1 para 1
+ *  1 para muitos
+ *  muitos para 1       -> @ManyToOne()
+ *  muitos para muitos 
+ */
