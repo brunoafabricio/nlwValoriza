78dda189c92b002c70a12177607c600150a7914f
Bruno <brunof.tech@gmail.com>
2021-06-24 13:58:43 -0300

    Compliments: Routes

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -17,4 +17,4 @@ class CreateComplimentController {
   }
 }
 
-export { CreateComplimentService }
+export { CreateComplimentController }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
@@ -10,12 +11,14 @@ const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
 
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 
 export { router } 
